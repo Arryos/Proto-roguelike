858cde36e09210c3cdaf199f252256876d93e283
Arryos <72743382+Arryos@users.noreply.github.com>
2026-02-27 19:24:39 +0100

    detection control device + Onlook fontion

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,24 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
 
-    //add SO later
     [SerializeField]
+    private GameObject body;
+
+    //GameData SO 
+    [SerializeField]
+    private SO_Float so_Speed;
+
     private float m_speed = 5;
     [SerializeField]
     private float jumpHeight = 2;
-    [SerializeField]
+
     private float gravity = -9.8f;
 
+    private bool isLookAtCursor = false;
 
     private Vector2 moveInput;
     private Vector3 velocity;
+    private Vector2 lookInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,12 +32,12 @@ public class PlayerController : MonoBehaviour
 
     private void OnEnable()
     {
-        
+        so_Speed.OnValueChanged += changeSpeed;
     }
 
     private void OnDisable()
     {
-        
+        so_Speed.OnValueChanged -= changeSpeed;
     }
 
     // Update is called once per frame
@@ -39,13 +46,76 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
         controller.Move(move * m_speed * Time.deltaTime);
 
-        velocity.y -= gravity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+
+
+        BodyDirection();
+
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        Debug.Log($"Move input : {moveInput}");
+        //Debug.Log($"Move input : {moveInput}");
+
+        if(!isLookAtCursor && moveInput != Vector2.zero)
+        {
+            LastDirection = moveInput;
+        }
+    }
+
+    private Vector2 LastDirection;
+
+      public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+        if(lookInput != Vector2.zero)
+        {
+            LastDirection = lookInput;
+            isLookAtCursor = true;
+        }
+        else
+        {
+            isLookAtCursor = false;
+        }
+        //Debug.Log($"Move input : {moveInput}");
+
+        //body.transform.LookAt(new Vector3(lookInput.x, transform.position.y, lookInput.y));
+    }
+
+    private int fireCnt = 0;
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire");
+
+        fireCnt++;
+        if(fireCnt%3 == 0)
+        {
+            Debug.Log("End Fire");
+            //isLookAtCursor = false;
+            fireCnt = 0;
+        }
+        else
+        {
+            Debug.Log("Active  Fire");
+            //isLookAtCursor = true;
+        }
+    }
+
+    private void BodyDirection()
+    {
+        //Get angle from lastDirection
+        float targetAngle = Mathf.Atan2(LastDirection.x, LastDirection.y) * Mathf.Rad2Deg;
+
+        body.transform.localRotation = Quaternion.Euler(0f, targetAngle, 0f);
+    }
+
+    private void changeSpeed(float p_speed)
+    {
+        m_speed = p_speed;
     }
 }
